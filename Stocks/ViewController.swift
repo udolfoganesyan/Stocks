7db3d72f786d0aca753a78354362843fb743ba5b
Rudolf Oganesyan <rude97@yandex.ru>
2020-08-28 21:32:11 +0300

    initial commit

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var companyNameLabel: UILabel!
+    @IBOutlet private weak var companySymbolLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    @IBOutlet private weak var priceChangeLabel: UILabel!
+    @IBOutlet private weak var companyPickerView: UIPickerView!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
+    private lazy var companies = [
+        "Apple": "AAPL"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        companyPickerView.dataSource = self
+        companyPickerView.delegate = self
     }
-
-
 }
 
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        companies.keys.count
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        Array(companies.keys)[row]
+    }
+}
