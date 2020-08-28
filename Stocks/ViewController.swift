d40d25b63151fb969fe2b94e4ba7256426a19aa6
Rudolf Oganesyan <rude97@yandex.ru>
2020-08-28 21:01:08 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Rudolf Oganesyan on 28.08.2020.
+//  Copyright © 2020 Рудольф О. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
